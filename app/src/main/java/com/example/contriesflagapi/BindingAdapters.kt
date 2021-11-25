0c8ac5c326bf3323d8b6045835116d8c578c2ce0
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-11-25 11:08:45 +0300

    test completed

--- app/src/main/java/com/example/contriesflagapi/BindingAdapters.kt
+++ app/src/main/java/com/example/contriesflagapi/BindingAdapters.kt
@@ -1,24 +1,44 @@
 package com.example.contriesflagapi
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.decode.SvgDecoder
 import coil.load
+import coil.request.ImageRequest
+
 import com.example.contriesflagapi.network.CountryModel
 import java.util.*
 
 
+@SuppressLint("UseCompatLoadingForDrawables")
 @BindingAdapter("photoUrl")
 fun ImageView.bind(photoUrl: String?){
     photoUrl?.let {
-    val photoUri = photoUrl.toUri().buildUpon().scheme("https").build()
-    this.load(photoUri) {
-        placeholder(R.drawable.loading_animation)
-        error(R.drawable.ic_broken_image)
+
+       val imageLoader = ImageLoader.Builder(this.context)
+           .componentRegistry { add(SvgDecoder(context)) }
+           .build()
+
+       val request = ImageRequest.Builder(this.context)
+           .crossfade(true)
+           .crossfade(500)
+           .data(photoUrl)
+           .target(this)
+           .build()
+
+       imageLoader.enqueue(request)
        }
+//        Glide.with(this.context)
+//            .load(photoUri)
+//            .placeholder(R.drawable.loading_animation)
+//            .error(R.drawable.ic_broken_image)
+//            .into(this)
     }
-}
+
 
 
 @BindingAdapter("listData")
